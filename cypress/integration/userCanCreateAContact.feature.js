1e2d010a769dbb2edf961cf0978b992a23a79db7
Rithreaksa <wonderfulreaksa@gmail.com>
2020-10-20 13:20:08 +0200

    Connect app.js to index.html and write two more 'it' blocks

--- cypress/integration/userCanCreateAContact.feature.js
+++ cypress/integration/userCanCreateAContact.feature.js
@@ -9,5 +9,14 @@ describe('user can create a contact', () => {
     cy.get('#company').type('Noah Trading Company')
     cy.get('#notes').type('Best friend')
     cy.get('#twitter').type('@pumpkin0430')
+    cy.get('#submit').click()
+  })
+
+  it('displays a name of the new contact', () => {
+    cy.get('#contact-list').should('contain', 'Thomas')
+  })
+
+  it('displays the phone number of the new contact', () => {
+    cy.get('#contact-list').should('contain', '076 450 04 05')
   })
 })
